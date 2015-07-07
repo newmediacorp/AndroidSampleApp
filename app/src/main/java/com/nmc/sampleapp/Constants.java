373a473991e6254816e0caeb31e1fd96ecf855e8
Rubin Gjerovski <rubin.gerovski@nmc.com.mk>
2015-07-07 13:55:31 +0200

    first commit

---
+++ app/src/main/java/com/nmc/sampleapp/Constants.java
@@ -0,0 +1,7 @@
+package com.nmc.sampleapp;
+
+public class Constants {
+
+    public static final String[] DRAWER_MENU = {"Item 1", "Item 2", "Item 3"};
+
+}
---
+++ app/src/main/java/com/nmc/sampleapp/MainActivity.java
@@ -0,0 +1,105 @@
+package com.nmc.sampleapp;
+
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle drawerToggle;
+    private ListView leftDrawerList;
+    private RelativeLayout workingAreaContainer;
+    private ArrayAdapter<String> navigationDrawerAdapter;
+    private String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initView();
+        if (toolbar != null) {
+            toolbar.setTitle(getString(R.string.title_activity_main));
+            setSupportActionBar(toolbar);
+        }
+
+        initDrawer();
+    }
+
+    private void initView() {
+        workingAreaContainer = (RelativeLayout) findViewById(R.id.workingAreaContainer);
+        leftDrawerList = (ListView) findViewById(R.id.left_drawer);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        navigationDrawerAdapter = new ArrayAdapter<String>(MainActivity.this, R.layout.drawer_list, Constants.DRAWER_MENU) {
+
+        };
+
+        leftDrawerList.setAdapter(navigationDrawerAdapter);
+        if(Constants.DRAWER_MENU.length > 0) {
+            leftDrawerList.setSelection(0);
+        }
+    }
+
+    private void initDrawer() {
+
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close) {
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+
+            }
+        };
+        drawerLayout.setDrawerListener(drawerToggle);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        if (drawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
